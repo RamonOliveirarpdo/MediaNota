f184c915200d4bd9075effc88e67fc1a157a1357
r-oliveira <r-oliveira@SUPORTE-88>
2022-07-20 16:10:07 -0300

    Add project files.

---
+++ media/Program.cs
@@ -0,0 +1,24 @@
+﻿decimal nota1, nota2, nota3, nota4, media;
+
+Console.WriteLine("Escreva 1 nota: ");
+nota1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Escreva 2 nota: ");
+nota2 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Escreva 3 nota: ");
+nota3 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Escreva 4 nota: ");
+nota4 = Convert.ToDecimal(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+Console.WriteLine("Sua media é: " + media);
+if (media > 7)
+        { Console.WriteLine("Reprovado"); }
+    else 
+        { Console.WriteLine("Aprovado"); }
+
+
+
